0154a9a8b8c4d57adc19c538c2df787bb1a5d395
debbyIT <deborah.oyelayo@stu.cu.edu.ng>
2026-08-09 18:50:56 +0100

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const { truncate } = require('node:fs');
 const app = express();
 const port = 3000;
 
+
+const tasks= [
+    { "id": 1, "title": "Write Code", "done": true },
+    { "id": 2, "title": "Test Code", "done": false },
+    { "id": 3, "title": "Deploy Code", "done": true }
+]
+
+//Home Page
 app.get('/', (req, res) => {
     res.json(
         { "name": "Task API", 
@@ -10,8 +19,27 @@ app.get('/', (req, res) => {
         })
 });
 
+//Health Check Endpoint
 app.get('/health',(req,res) =>{
     res.json({"status": "ok"})
+});
+
+//Get All Tasks
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+//Get Task by ID
+app.get('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+
+    if (task) {
+        res.status(200).json(task);
+    } else {
+        res.status(404).json({ "error": `Task ${taskId} not found` });
+    }
+
 });
 
 app.listen(port, ()=>{
