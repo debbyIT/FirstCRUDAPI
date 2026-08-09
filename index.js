8bcd6be3e56cd8856cc1c27ba0bbbbb71de1d852
debbyIT <deborah.oyelayo@stu.cu.edu.ng>
2026-08-09 18:40:27 +0100

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    console.log('Hello World');
-    res.send('Hello World');
+    res.json(
+        { "name": "Task API", 
+            "version": "1.0", 
+            "endpoints": ["/tasks"] 
+        })
+});
+
+app.get('/health',(req,res) =>{
+    res.json({"status": "ok"})
 });
 
 app.listen(port, ()=>{
